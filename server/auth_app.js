73400b26014d51f094e315cc86fe7d1335b8292b
loinq <loinq@metechvn.com>
2021-03-25 17:37:47 +0700

    add api for report-request-recall

--- server/auth_app.js
+++ server/auth_app.js
@@ -83,6 +83,7 @@ function initServer(db, dbMssql) {
     app.use("/api/v1/reportTCDGroupby", require("./routes/reportTCDGroupbyRoute"));
     app.use("/api/v1/reportCustomize", require("./routes/reportCustomizeRoute"));
     app.use("/api/v1/callDetail", require("./routes/callDetailRoute"));
+    app.use("/api/v1/", require("./routes/reportRequestRecallRoute"));
     app.use("/api/v1/reportRealTime", require("./routes/reportRealTimeRoute"));
     app.use("/api/v1/reasonCode", require("./routes/reasonCodeRoute"));
     app.use("/api/v1/survey", require("./routes/dialedNumberRoute"));
