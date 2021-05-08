27db27b429845eca60e5fded0b7fc0fcfe6da88c
Nguyen Van Hai (Team Lead) <hainv@metechvn.com>
2021-05-08 10:13:16 +0700

    fix data inbound handle with talktime params

--- server/models/dataTCDInboundModal.js
+++ server/models/dataTCDInboundModal.js
@@ -19,13 +19,13 @@ exports.lastTCDRecord = async (db, dbMssql, query) => {
     const nameTableTCDLast = `t_TCD_last`;
 
     if (queue) {
-      queryQueue += `Where TempTable2.SkillGroupSkillTargetID IN (${queue})`
+      queryQueue += `Where ${nameTableTCDLast}.SkillGroupSkillTargetID IN (${queue})`
     }
     if (status) {
       if (queryQueue != "") {
-        queryStatus += `AND TempTable2.DIRECTION IN (${status})`
+        queryStatus += `AND ${nameTableTCDLast}.DIRECTION IN (${status})`
       } else {
-        queryStatus += `WHERE TempTable2.DIRECTION IN (${status})`
+        queryStatus += `WHERE ${nameTableTCDLast}.DIRECTION IN (${status})`
       }
     }
 
@@ -38,7 +38,7 @@ exports.lastTCDRecord = async (db, dbMssql, query) => {
                 OFFSET ${(pages - 1) * rows} ROWS FETCH NEXT ${rows} ROWS ONLY
             `;
     }
-    _logger.log("info", `querySelect ${querySelect}`);
+    // _logger.log("info", `querySelect ${querySelect}`);
     Object.keys(query).forEach((item) => {
       const element = query[item];
       if (item.includes("CT")) {
@@ -47,6 +47,14 @@ exports.lastTCDRecord = async (db, dbMssql, query) => {
     });
 
     let _query = `
+    /**
+      Mô tả:
+        - Start: 08/05/2021
+        - Detail: danh sách các cuộc gọi handle theo 1 bộ CallType và SkillGroup, thỏa mãn điều kiện talktime >= xxx
+        
+        Log 1: 08/05/2021 ....
+      */
+
         ${variableSQLDynamic(query)}
         WITH ${nameTableTCDLast} AS (
             SELECT ROW_NUMBER() OVER (PARTITION BY  RouterCallKeyDay, RouterCallKey ORDER BY RouterCallKeySequenceNumber DESC, RecoveryKey DESC) AS rn
@@ -59,7 +67,7 @@ exports.lastTCDRecord = async (db, dbMssql, query) => {
         ${querySelect}
         ${queryQueue}
         ${queryStatus}
-        ${parseInt(query.flag) != 3 ? "ORDER BY TempTable2.DateTime DESC" : ""}
+        ${parseInt(query.flag) != 3 ? `ORDER BY  ${nameTableTCDLast}.DateTime DESC` : ""}
         ${queryPage}`;
 
     _logger.log("info", `lastTCDRecord ${_query}`);
@@ -110,77 +118,26 @@ function selectCallDetailByCustomer(query, nameTable) {
     }
   });
 
-  switch (parseInt(query.flag)) {
-    case 2:
-      return `
-            SELECT * from (
-                SELECT TempTable.DateTime,TempTable.startTime,TempTable.ANI,TempTable.SkillGroupSkillTargetID,TalkTime
-                FROM(
-                    SELECT
-                    [DateTime]
-                    ,[Duration]
-                    ,DATEADD(ss , -Duration, DateTime) AS startTime
-                    ,[DigitsDialed]
-                    ,[RecoveryKey]
-                    ,[TalkTime]
-                    ,CASE
-                    ${when_dynamic.join("")}
-                        else SkillGroupSkillTargetID
-                    end SkillGroupSkillTargetID
-                    ,[ANI]
-                FROM ${nameTable}
-                    WHERE DateTime >= @startDate
-                    AND DateTime < @endDate
-                    AND TalkTime >= ${+query.talkTime}
-                    AND rn = 1 --lấy cuộc gọi cuối cùng
-                    AND ${nameTable}.RecoveryKey not in (
-                        Select RecoveryKey FROM [${DB_HDS}].[dbo].[t_Termination_Call_Detail] t_TCD_handle
-                        where  DateTime >= @startDate
-                        AND DateTime < @endDate
-                        AND (
-                            -- loại các cuộc handle
-                            AgentSkillTargetID is not null
-                            AND t_TCD_handle.CallTypeID in (${[...CT_ToAgent_Dynamic, ...CT_Queue_Dynamic].join(",")})
-                            AND TalkTime > 0
-                            AND CallDisposition in (13,28)
-                        )
-                    )
-                ) TempTable ) TempTable2`;
-    case 3:
-      return `
-            SELECT count(*) as totalPage from (
-                SELECT TempTable.DateTime,TempTable.startTime,TempTable.ANI,TempTable.SkillGroupSkillTargetID,TalkTime
-                FROM(
-                    SELECT
-                    [DateTime]
-                    ,[Duration]
-                    ,DATEADD(ss , -Duration, DateTime) AS startTime
-                    ,[DigitsDialed]
-                    ,[RecoveryKey]
-                    ,[TalkTime]
-                    ,CASE
-                    ${when_dynamic.join("")}
-                        else SkillGroupSkillTargetID
-                    end SkillGroupSkillTargetID
-                    ,[ANI]
-                FROM ${nameTable}
-                    WHERE DateTime >= @startDate
-                    AND DateTime < @endDate
-                    AND TalkTime >= ${+query.talkTime}
-                    AND rn = 1 --lấy cuộc gọi cuối cùng
-                    AND ${nameTable}.RecoveryKey not in (
-                        Select RecoveryKey FROM [${DB_HDS}].[dbo].[t_Termination_Call_Detail] t_TCD_handle
-                        where  DateTime >= @startDate
-                        AND DateTime < @endDate
-                        AND (
-                            -- loại các cuộc handle
-                            AgentSkillTargetID is not null
-                            AND t_TCD_handle.CallTypeID in (${[...CT_ToAgent_Dynamic, ...CT_Queue_Dynamic].join(",")})
-                            AND TalkTime > 0
-                            AND CallDisposition in (13,28)
-                        )
-                    )
-                ) TempTable ) TempTable2`;
-  }
+  return `SELECT
+  [DateTime]
+  ,[Duration]
+  ,DATEADD(ss , -Duration, DateTime) AS startTime
+  ,[DigitsDialed]
+  ,[RecoveryKey]
+  ,[TalkTime]
+  ,CASE
+  ${when_dynamic.join("")}
+      else SkillGroupSkillTargetID
+  end SkillGroupSkillTargetID
+  ,[ANI]
+  FROM ${nameTable}
+    WHERE ${nameTable}.DateTime >= @startDate
+    AND ${nameTable}.DateTime < @endDate
+    AND ${nameTable}.rn = 1 --lấy cuộc gọi cuối cùng
+    -- các cuộc handle
+    AND ${nameTable}.AgentSkillTargetID is not null
+    AND ${nameTable}.CallTypeID in (${[...CT_ToAgent_Dynamic, ...CT_Queue_Dynamic].join(",")})
+    AND ${nameTable}.TalkTime >= ${+query.talkTime}
+    AND ${nameTable}.CallDisposition in (13,28)`
 }
 
