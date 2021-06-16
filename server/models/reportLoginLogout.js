9a56516ee5d8e718a14909864e7c4ea77861f5ba
phamtinh142 <phamtinh142@gmail.com>
2021-06-16 18:23:31 +0700

    report login - logout per time

--- server/models/reportLoginLogout.js
+++ server/models/reportLoginLogout.js
@@ -15,6 +15,12 @@ exports.reportLoginLogout = async (db, dbMssql, body) => {
       `;
     }
 
+    if (type == 'by-time') {
+      _query = `
+        USE tempdb exec dev_report_login_logout_per_time '${startTime}', '${endTime}', '${agentTeams}', '${agents || '#'}'
+      `;
+    }
+
     console.info(`------- _query ------- reportLoginLogout`);
     console.info(_query);
     console.info(`------- _query ------- reportLoginLogout`);
