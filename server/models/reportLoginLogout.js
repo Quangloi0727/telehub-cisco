ec2c9dcf77b26ed4850ab8540ac29d365438b632
phamtinhdev <phamtinh.dev@gmail.com>
2021-09-15 08:16:37 +0700

    report login - logout v2

--- server/models/reportLoginLogout.js
+++ server/models/reportLoginLogout.js
@@ -30,3 +30,58 @@ exports.reportLoginLogout = async (db, dbMssql, body) => {
     throw new Error(error);
   }
 };
+
+/**
+ * API này có chức năng lấy ra các trạng thái và thời gian login logout của agent
+ * Vì sao có API này:
+ * - API này được dựa trên API 'report-login-logout'
+ * - Vì có một số dự án yêu cầu lấy ra các loại Status khác nhau như:
+ *    + AtLunch
+ *    + Meeting
+ *    + NoACD
+ *    + NoAvailable
+ *    + Not Ready
+ *    + Training
+ *    + Xu Ly Phieu
+ * - Vì vậy API này được tạo ra để nhằm đáp ứng nhu cầu của khách hàng
+ * @param {*} dbMssql 
+ * @param {*} body 
+ * @returns 
+ */
+exports.reportChangeStatus = async (dbMssql, body) => {
+  try {
+    const { type, startTime, endTime, agentTeams, agents } = body;
+
+    let childQuery = '';
+
+    if (type == 'total') {
+      childQuery = 'EXEC dev_report_login_logout_total_new_sp @p_startTime ,@p_endTime ,@p_agentTeam ,@p_agentId';
+    }
+
+    if (type == 'by-day') {
+      childQuery = 'EXEC dev_report_login_logout_per_day_new_sp @p_startTime ,@p_endTime ,@p_agentTeam ,@p_agentId'
+    }
+
+    if (type == 'by-time') {
+      childQuery = 'EXEC dev_report_login_logout_per_time_new_sp @p_startTime ,@p_endTime ,@p_agentTeam ,@p_agentId';
+    }
+
+    let _query = `
+      DECLARE @p_startTime datetime = '${startTime}'
+      DECLARE @p_endTime datetime = '${endTime}'
+      DECLARE @p_agentTeam varchar(2000) = '${agentTeams}'
+      DECLARE @p_agentId varchar(2000) = '${agents || '#'}'
+      
+      USE tempdb
+      ${childQuery}
+    `;
+
+    console.info(`------- _query ------- reportChangeStatus`);
+    console.info(_query);
+    console.info(`------- _query ------- reportChangeStatus`);
+
+    return await dbMssql.query(_query);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
